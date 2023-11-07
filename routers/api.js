6f37bfb1bb23eb20f26e36984e948eb96205b12c
Hendar <hendarsundana@gmail.com>
2023-11-07 21:05:35 +0700

    feat: add api project and features

--- routers/api.js
+++ routers/api.js
@@ -2,6 +2,25 @@ const express = require('express');
 const webchannel = require('./api/webchannel');
 const api = express.Router();
 
+
+const features = require('./../databases/features.json');
+const projects = require('./../databases/projects.json');
+
+
 api.use('/', webchannel);
+
+api.use('/v1/features', (req, res) => {
+    res.status(200).json({
+        message : "data features",
+        data: features
+    })
+})
+api.use('/v1/projects', (req, res) => {
+    res.status(200).json({
+        message : "data project",
+        data: projects
+    })
+})
+
 
 module.exports = api;
